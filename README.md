f5110010d0f2f2fb7613b2b5d3e31144edd742b1
trevery <canjia.mail@gmail.com>
2017-06-22 14:15:02 +0800

    chage some files' name

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ let alpha 1 dream a dream
 
 ## take some photos with `raspstill`
 
-
 example： 
 
 `raspistill -o frame%04d.jpg -tl 10000 -t 60000`
@@ -16,5 +15,30 @@ Photos will be saved as frame0000.jpg frame0001.jpg.
 `frame.findHaarFeatures('face')` will return a list
 contain faces' x,y coordinates.
 
-## convert face-photos into a video with "avcon"
+and i wrote a python script named faceFilter.py to find if there are face in the photo. 
+
+
+## deep dream
+
+`pi@alpha_pi3:~/deepdream/PsyCam $ python psycam.py -i imgDir.jpg`
+
+I have wrote an alias for this python command. 
+`alias dream='python ~/deepdream/PsyCam/psycam.py`
+
+While, I found only in the directory '~/deepdream/PsyCam', 
+this command can execute rightly. In other words, 
+if you in ~ directory, it will throw fault.
+
+
+
+## convert face-photos into a video with "avconv"
+
+### install
+
+`sudo apt-get install libav-tools`
+
+### example:
+`avconv -r 10 -i frame%03d.jpg -r 10 -vcodec libx264 timelapse.mp4`
+
+ 
 
--- bin/dad.py
+++ bin/dad.py
@@ -1,1 +1,61 @@
+#! /usr/bin/env python
+
+'''
+To: get photos from '../photos/takeSomePhotos/'
+Author: trevery
+Time: 20, Jun, 2017
+'''
+
+import os
+import subprocess
+imgDir = '/home/pi/alpha-dad/photos/takeSomePhotos/'
+
+def getImageCount(imgDir):
+	imgList = os.listdir(imgDir)
+	imgCount = len(imgList)
+	return imgCount
+
+def getImageList(imgDir):
+	imgList = os.listdir(imgDir)
+	return imgList
+	
+def getAbsDir(imgName):
+	imgAbsDir = imgDir+imgName
+	return imgAbsDir	
+	
+imgList = getImageList(imgDir)
+#print imgList
+imgCount = getImageCount(imgDir)
+#print imgCount
+for i in range(0,imgCount):
+	imgAbsDir = getAbsDir(imgList[i])
+	
+	#print('image:'+imgAbsDir)
+	# dream only can be use rightly in directory
+	# `~/deepdream/PsyCam/`
+	#os.curdir(".")
+	
+	os.chdir('/home/pi/deepdream/PsyCam/')
+	
+	#os.system('cd ~/deepdream/PsyCam/')
+	#print(os.getcwd())
+	#os.system('pwd')
+	#subprocess.call('python psycam.py -i'+'imgAbsDir')
+	
+	
+	##dream
+	#os.system('python psycam.py -i '+imgAbsDir)
+
+##format images' name
+imgList = getImageList(imgDir)
+imgCount = getImageCount(imgDir)
+for i in range(0,imgCount):
+	imgName = imgList[i]
+	print ('imgName: ' + imgName)
+	print(('mv '+imgName+' '+'frame00'+str(i)+'.jpg'))
+	os.chdir('/home/pi/alpha-dad/photos/takeSomePhotos/')
+	#os.getcwd()
+	os.system('mv '+imgName+' '+'frame'+('%03d' %i)+'.jpg')
+	
+	
 
